0114b365e7448f713f87f52824e604394b563047
Arvind Batham <arvindbatham600@gmail.com>
2024-04-16 03:02:08 +0530

    implemented debouncing

--- week4/DOM_Project1/index.js
+++ week4/DOM_Project1/index.js
@@ -1,20 +1,28 @@
+let timeout;
+
+const deBouncing = () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    calculateSum();
+  }, 1000);
+};
+
 const calculateSum = () => {
   const firstNumber = parseFloat(document.getElementById("firstNumber").value);
   const secondNumber = parseFloat(
     document.getElementById("secondNumber").value
   );
-    fetch("https://sum-server.100xdevs.com/sum?a=" + firstNumber + "&b=" + secondNumber + " ").then((response) => {
-        console.log(response);
-        response.text().then((ans) => {
-            console.log(ans)
-        })
-    })
-//   const sum = firstNumber + secondNumber;
-//   const answerField = document.getElementsByClassName("answer")[0];
-//   answerField.innerHTML = "Answer : " + sum;
+  fetch(
+    "https://sum-server.100xdevs.com/sum?a=" +
+      firstNumber +
+      "&b=" +
+      secondNumber +
+      " "
+  ).then((response) => {
+    console.log(response);
+    response.text().then((ans) => {
+        console.log(ans);
+        document.getElementById("answer").innerHTML = ans
+    });
+  });
 };
-const button = document.getElementsByClassName("sum")[0]; 
-button.addEventListener("click", calculateSum);
-
-
-// calculateSum();&
