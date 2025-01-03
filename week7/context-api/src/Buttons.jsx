fe8cb7b7e2cc8ac45e6abd38eb3e859b7012f1e3
Arvind Batham <arvind@Arvinds-MacBook-Air.local>
2025-01-03 16:37:35 +0530

    button re-rendering stopped

--- week7/context-api/src/Buttons.jsx
+++ week7/context-api/src/Buttons.jsx
@@ -1,15 +1,15 @@
 // import { useContext } from "react";
 // import { CountContext } from "./context";
-// import { useSetRecoilState } from "recoil";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
+// import { useRecoilState } from "recoil";
 import { countAtom } from "./store/atoms/count";
 
 const Buttons = () => {
-  const [count, setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count => count + 1)}>Increase</button>
+      <button onClick={() => setCount(count => count - 1)}>Decrease</button>
     </>
   );
 };
